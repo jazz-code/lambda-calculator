8a1dc0dc5f4dc43d19edbc01b28fcdd3e35c98fd
jazz-code <ren.meta@gmail.com>
2019-07-16 22:13:09 -0400

    Switched css to sass, changed file directores

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
-// STEP 4 - import the button and display components
-// Don't forget to import any extra css/scss files you build into the correct component
-
-// Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
 import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
+import Display from "./components/DisplayComponents/Display";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -19,11 +16,14 @@ function App() {
   return (
     <div className="container">
       <Logo />
+      <Display />
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-        <Numbers />
-        <Operators />
-        <Specials />
+        <div className="btn-container">
+          {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+          <Numbers />
+          <Operators />
+          <Specials />
+        </div>
       </div>
     </div>
   );
